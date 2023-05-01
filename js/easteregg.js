4d42091fb38e443626934ead73d2c026ccb12400
Baptiste Gosselin <49567444+BaptGosse@users.noreply.github.com>
2023-05-01 20:24:04 +0200

    Update a bug in easteregg

--- js/easteregg.js
+++ js/easteregg.js
@@ -4,6 +4,7 @@ buttonEasterEgg.addEventListener("click", easteregg)
 croixEasterEgg.addEventListener("click", closegg)
 function easteregg(){
     document.querySelector("main section#cloud article aside").style.display = "block";
+    document.querySelector("main section#cloud article aside div video").play();
 }
 function closegg() {
     document.querySelector("main section#cloud article aside").style.display = "none";

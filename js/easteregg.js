f198cd31d76e23087167d7e730909bd6838f5179
Baptiste Gosselin <49567444+BaptGosse@users.noreply.github.com>
2023-05-01 20:15:58 +0200

    Responsive and integration

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,3 +1,7 @@
+const buttonEasterEgg = document.querySelector("main section#cloud article p a");
+const croixEasterEgg = document.querySelector("main section#cloud article aside div span");
+buttonEasterEgg.addEventListener("click", easteregg)
+croixEasterEgg.addEventListener("click", closegg)
 function easteregg(){
     document.querySelector("main section#cloud article aside").style.display = "block";
 }
